1b9fd1934670fe93022d21acc035d303ba850f0e
Vitaliy Suhoplechev <vitaliy.suhoplechev.1@gmail.com>
2025-10-25 13:17:17 +0300

    Create main.c
    За первый коммит я решил взять то, с чего начинается видео. Разобрался, что mapWidth + 1 нужен скорее всего для нулевого итератора

---
+++ main.c
@@ -0,0 +1,17 @@
+//За первый коммит я решил взять то, с чего начинается видео.
+#include <stdio.h>
+#include <stdlib.h>
+
+#define mapWidth 80
+#define mapHeight 25
+
+char map[mapHeight][mapWidth + 1]; // МОЙ КОММЕНТАРИЙ: +1 for null terminator, это значит что строка заканчивается символом '\0'
+
+void ClearMap() {
+    for
+}
+
+int main() {
+
+    return 0;
+}
