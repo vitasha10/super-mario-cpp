4daf7875ad52f9d419b3ff2f3b2b54fbc3f5c501
Vitaliy Suhoplechev <vitaliy.suhoplechev.1@gmail.com>
2025-11-11 15:07:35 +0300

    can kill enemy
    Теперь можно убивать врагов, прыгая на них сверху

--- main.c
+++ main.c
@@ -121,13 +121,30 @@ void VertMoveObject(TObject *obj)
     }
 }
 
+void DeleteMoving(int i)
+{
+    movingLength--;
+    moving[i] = moving[movingLength];
+    moving = realloc(moving, sizeof(*moving) * movingLength);
+}
+
 void MarioCollision()
 {
     for(int i = 0; i < movingLength; i++)
     {
         if(IsCollision(mario, moving[i]))
         {
-            CreateLevel(level);
+            if(    (mario.IsFly == TRUE) 
+                && (mario.vertSpeed > 0) 
+                && (mario.y + mario.height < moving[i].y + moving[i].height * 0.5)
+                ) 
+            {
+                DeleteMoving(i); 
+                i--;
+                continue;
+            } 
+            else 
+                CreateLevel(level);
         }
     }
 }
